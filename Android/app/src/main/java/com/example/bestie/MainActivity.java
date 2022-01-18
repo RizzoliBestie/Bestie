914230076cea7c8d14c3355fed26e5ddaa63fe0f
reeccardo <94625621+reeccardo@users.noreply.github.com>
2022-01-18 17:18:32 +0100

    Risolto conflitto manifest

---
+++ Android/app/src/main/java/com/example/bestie/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.bestie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.GridView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        GridView page_select = findViewById(R.id.page_select);
+    }
+}
