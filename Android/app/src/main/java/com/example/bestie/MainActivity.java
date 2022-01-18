7f67b6fbfa6e0a5a9da8ac9c3db0c5b422fab6ce
reeccardo <94625621+reeccardo@users.noreply.github.com>
2022-01-18 17:30:22 +0100

    Grid view per cambiare pagina in home. File xml e java, struttura base

--- Android/app/src/main/java/com/example/bestie/MainActivity.java
+++ Android/app/src/main/java/com/example/bestie/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.bestie;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.GridView;
+import android.widget.ListAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +15,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         GridView page_select = findViewById(R.id.page_select);
+        String[] vociMenu = new String[]{"Home", "Map", "Curiosità"};
+        ListAdapter menuAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, vociMenu);
+        page_select.setAdapter(menuAdapter);
     }
 }
