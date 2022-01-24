324f8094ef79e9b289fb5c493c2531567ab7703b
Mike7373 <61974139+Mike7373@users.noreply.github.com>
2022-01-24 16:26:35 +0100

    Aggiunta activity
    Aggiunte tutte le activity del progetto

---
+++ Android/app/src/main/java/com/example/bestie/Map.java
@@ -0,0 +1,14 @@
+package com.example.bestie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Map extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+}
