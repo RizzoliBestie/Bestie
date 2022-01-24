bfdad42cfd787366950f5002f190fb88b6c874ca
Mike7373 <61974139+Mike7373@users.noreply.github.com>
2022-01-24 17:11:17 +0100

    Login
    Aggiunta la schermata di login, impostata sulla base del sign up

--- Android/app/src/main/java/com/example/bestie/Activity/LogInActivity.java
+++ Android/app/src/main/java/com/example/bestie/Activity/LogInActivity.java
@@ -3,6 +3,10 @@ package com.example.bestie.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.bestie.R;
 
@@ -12,6 +16,23 @@ public class LogInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        Button confirm_button = findViewById(R.id.confirm_button);
+        EditText username =  findViewById(R.id.username);
+        EditText email =  findViewById(R.id.email);
+
+        CharSequence tUsername = username.getText();
+        CharSequence tEmail = email.getText();
+
+        confirm_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (tUsername.length()==0||tEmail.length()==0)
+                    Toast.makeText(LogInActivity.this, "Please fill in all fields", Toast.LENGTH_LONG).show();
+                else
+                    Toast.makeText(LogInActivity.this, "You're logged in now", Toast.LENGTH_LONG).show();
+            }
+        });
 
 
     }
