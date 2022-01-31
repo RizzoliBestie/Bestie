442d798a89571c2998c8a4ed995664d8cf60644e
lufoxferrari <l.ferrari@itsrizzoli.it>
2022-01-31 17:56:21 +0100

    Creazione classe generica Animale
    Utile per essere implementata su diverse feature, (tra cui la ListView Adapter nelle curiosity)

---
+++ Android/app/src/main/java/com/example/bestie/Activity/Animal.java
@@ -0,0 +1,14 @@
+package com.example.bestie.Activity;
+
+public class Animal {
+    //Classe base per gli animali
+    String name;
+    String race;
+    String specie;
+
+    Animal(String n, String r, String s){
+        this.name = n;
+        this.race = r;
+        this.specie = s;
+    }
+}
