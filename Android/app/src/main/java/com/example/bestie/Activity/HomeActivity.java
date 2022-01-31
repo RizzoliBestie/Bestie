281b5a9816c133cb6da59bff371063a8a1556cd1
reeccardo <94625621+reeccardo@users.noreply.github.com>
2022-01-31 16:32:46 +0100

    Elimino main activity  file xml perché sostituita con HomeActivity

--- Android/app/src/main/java/com/example/bestie/Activity/HomeActivity.java
+++ Android/app/src/main/java/com/example/bestie/Activity/HomeActivity.java
@@ -3,6 +3,9 @@ package com.example.bestie.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.ListAdapter;
 
 import com.example.bestie.R;
 
@@ -12,5 +15,10 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        GridView page_select = findViewById(R.id.page_select);
+        String[] vociMenu = new String[]{"Home", "Map", "Curiosità"};
+        ListAdapter menuAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, vociMenu);
+        page_select.setAdapter(menuAdapter);
     }
 }
