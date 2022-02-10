c298255febef84eaf6a1684246d3f7116e99e6f7
reeccardo <94625621+reeccardo@users.noreply.github.com>
2022-02-10 15:27:23 +0100

    slider menù
    aggiunta slider menù in home activity, aggiunta classe ContentSlider e due file xml

---
+++ Android/app/src/main/java/com/example/bestie/home/ContentSlider.java
@@ -0,0 +1,24 @@
+package com.example.bestie.home;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.bestie.R;
+import com.yarolegovich.slidingrootnav.SlidingRootNavBuilder;
+
+public class ContentSlider extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sliding_layout);
+        new SlidingRootNavBuilder(this)
+                .withMenuOpened(false)
+                .withContentClickableWhenMenuOpened(false)
+                .withSavedState(savedInstanceState)
+                .withMenuLayout(R.layout.drawer_menu)
+                .inject();
+    }
+}
