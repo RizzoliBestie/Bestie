16142a7e28048eedb2d5d3a3b1cfd84ab48c39d6
reeccardo <94625621+reeccardo@users.noreply.github.com>
2022-02-13 11:32:27 +0100

    update home activity e drawer_menu.xml

--- Android/app/src/main/java/com/example/bestie/home/HomeActivity.java
+++ Android/app/src/main/java/com/example/bestie/home/HomeActivity.java
@@ -2,15 +2,18 @@ package com.example.bestie.home;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.GridView;
 import android.widget.ListAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.bestie.R;
+import com.example.bestie.settings.SettingsActivity;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -22,6 +25,9 @@ public class HomeActivity extends AppCompatActivity {
         GridView page_select = findViewById(R.id.page_select);
         Button menuButton = findViewById(R.id.menu_button);
         Button addPetButton = findViewById(R.id.addPetButton);
+        TextView archivio = findViewById(R.id.archivio);
+        TextView settings = findViewById(R.id.settings);
+        TextView logout = findViewById(R.id.logout);
 
         String[] vociMenu = new String[]{"Home", "Map", "Curiosità"};
         ListAdapter menuAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, vociMenu);
@@ -31,6 +37,14 @@ public class HomeActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Toast.makeText(HomeActivity.this, "Aggiunto", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent openSettings = new Intent(HomeActivity.this, SettingsActivity.class);
+                startActivity(openSettings);
             }
         });
     }
