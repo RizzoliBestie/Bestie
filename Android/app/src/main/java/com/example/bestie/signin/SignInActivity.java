15621a712fed68756fa06b6b672fc7481bfc17cb
Michael Bellotti <94625220+MichaelBellotti@users.noreply.github.com>
2022-02-23 17:57:00 +0100

    [+] Aggiunta Regisrazine
    Aggiunta la prima parte della registtrazione

--- Android/app/src/main/java/com/example/bestie/signin/SignInActivity.java
+++ Android/app/src/main/java/com/example/bestie/signin/SignInActivity.java
@@ -7,37 +7,157 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.bestie.API.API_Connection_Bestie;
+import com.example.bestie.API.API_Methods_Interface;
 import com.example.bestie.R;
 import com.example.bestie.login.LogInActivity;
 import com.example.bestie.settings.SettingsActivity;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+
 public class SignInActivity extends AppCompatActivity {
 
+    Button confirm_button;
+    EditText username, email, psw;
+    String tUsername, tPsw,tEmail;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign);
 
-        Button confirm_button = findViewById(R.id.confirm_button);
+        confirm_button = findViewById(R.id.confirm_button);
 
-        EditText username =  findViewById(R.id.username);
-        EditText email =  findViewById(R.id.email);
-        EditText psw =  findViewById(R.id.psw);
+        username =  findViewById(R.id.username);
+        email =  findViewById(R.id.email);
+        psw =  findViewById(R.id.psw);
 
-        CharSequence tUsername = username.getText();
-        CharSequence tEmail = email.getText();
-        CharSequence tPsw = psw.getText();
+        username.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
-        SharedPreferences pref = this.getSharedPreferences("preferences", Context.MODE_PRIVATE);
+            }
 
-        SharedPreferences.Editor edt = pref.edit();
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                tUsername = username.getText().toString();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        email.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                tEmail = email.getText().toString();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        psw.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                tPsw = psw.getText().toString();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+
 
         confirm_button.setOnClickListener(new View.OnClickListener() {
+
+            Retrofit retrofit = ((API_Connection_Bestie)getApplication()).getRetrofit();
+            API_Methods_Interface api = retrofit.create(API_Methods_Interface.class);
+
+            @Override
+            public void onClick(View view) {
+                if(tUsername.length()==0||tPsw.length()==0||tEmail.length()==0)
+                    Toast.makeText(SignInActivity.this, "Compila tutti i campi", Toast.LENGTH_LONG).show();
+                else if(tUsername.contains(" "))
+                    Toast.makeText(SignInActivity.this,"Lo username non può contenere spazi", Toast.LENGTH_LONG).show();
+                else if(tUsername.length()<=3)
+                    Toast.makeText(SignInActivity.this, "Lo username deve avere una lunghezza minima di 4 caratteri", Toast.LENGTH_LONG).show();
+                else if(tPsw.length()<=5)
+                    Toast.makeText(SignInActivity.this, "La password deve avere una lunghezza minima di 6 caratteri", Toast.LENGTH_LONG).show();
+                else if(tEmail.contains("@"))
+                    Toast.makeText(SignInActivity.this, "Inserisci un indirizzo email valido", Toast.LENGTH_LONG).show();
+                else{
+                    Call<Boolean> checkSigninCall = api.checkSignin(tUsername, tEmail);
+                    checkSigninCall.enqueue(new Callback<Boolean>() {
+                        @Override
+                        public void onResponse(Call<Boolean> call, Response<Boolean> response) {
+                            if (response.body()) {
+
+                                Toast.makeText(SignInActivity.this, "Email o nome utente già utilizzati!",Toast.LENGTH_LONG).show();
+
+                            } else {
+                                Call<Boolean> registerCall = api.register(tUsername, tEmail, tPsw, null);
+                                registerCall.enqueue(new Callback<Boolean>() {
+                                    @Override
+                                    public void onResponse(Call<Boolean> call, Response<Boolean> response) {
+                                        if(response.body()){
+                                            Toast.makeText(SignInActivity.this, "Registrazione avvenuta con successo!", Toast.LENGTH_LONG).show();
+
+                                            Intent moveToSettings = new Intent(SignInActivity.this, LogInActivity.class);
+                                            startActivity(moveToSettings);
+                                        } else {
+                                            Toast.makeText(SignInActivity.this, "Ops!! Qualcosa è andato storto!!", Toast.LENGTH_LONG).show();
+                                        }
+                                    }
+
+                                    @Override
+                                    public void onFailure(Call<Boolean> call, Throwable t) {
+                                        t.printStackTrace();
+                                    }
+                                });
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Call<Boolean> call, Throwable t) {
+                            t.printStackTrace();
+                        }
+                    });
+                }
+
+            }
+        });
+
+        //SharedPreferences pref = this.getSharedPreferences("preferences", Context.MODE_PRIVATE);
+
+        //SharedPreferences.Editor edt = pref.edit();
+
+        /*confirm_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (tUsername.length()==0||tEmail.length()==0||tPsw.length()==0)
@@ -64,7 +184,7 @@ public class SignInActivity extends AppCompatActivity {
                     startActivity(moveToSettings);
                 }
             }
-        });
+        });*/
     }
 
 }
