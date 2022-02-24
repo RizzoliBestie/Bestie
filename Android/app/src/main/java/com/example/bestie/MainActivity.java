e57dfa7025d7578fc7a81e57acb682ffd1329e71
reeccardo <94625621+reeccardo@users.noreply.github.com>
2022-02-24 16:01:10 +0100

    drawer menù senza funzioni

--- Android/app/src/main/java/com/example/bestie/MainActivity.java
+++ Android/app/src/main/java/com/example/bestie/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.bestie;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
@@ -22,31 +24,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-            Button menuButton = findViewById(R.id.menu_button);
-            BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_nav);
-            getSupportActionBar().hide();
+        Button menuButton = findViewById(R.id.menu_button);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        //DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
 
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
-            bottomNavigationView.setSelectedItemId(R.id.navigation_home);
-            bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-                @Override
-                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                    Fragment selected =null;
-                    switch (item.getItemId()){
-                        case R.id.navigation_home:
-                            selected=new HomeFragment();
-                            break;
-                        case R.id.navigation_map:
-                            selected=new MapFragment();
-                            break;
-                        case R.id.navigation_curiosity:
-                            selected=new CuriosityFragment();
-                            break;
-                    }
-                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selected).commit();
-                    return true;
+        setSupportActionBar(toolbar);
+        getSupportActionBar().hide();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+        bottomNavigationView.setSelectedItemId(R.id.navigation_home);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selected = null;
+                switch (item.getItemId()) {
+                    case R.id.navigation_home:
+                        selected = new HomeFragment();
+                        break;
+                    case R.id.navigation_map:
+                        selected = new MapFragment();
+                        break;
+                    case R.id.navigation_curiosity:
+                        selected = new CuriosityFragment();
+                        break;
                 }
-            });
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selected).commit();
+                return true;
+            }
+        });
 
-        }
+    }
+
+   /* @Override
+    public void onBackPressed() {
+        if ()
+        super.onBackPressed();
+    }*/
 }
