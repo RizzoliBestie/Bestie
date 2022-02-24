9f4ddaf38848f6114ae04bd65089abfc5edaff65
Lorenzo Arecco <l.arecco@itsrizzoli.it>
2022-02-24 20:28:01 +0100

    Piango perché ho perso dei file
    ma domani sistemo no worries(erano miei file di oggi)
    Io i conflitti li...
    ODIO.

---
+++ Android/app/src/main/java/com/example/bestie/storage/ItemGridAdapter.java
@@ -0,0 +1,4 @@
+package com.example.bestie.storage;
+
+public class ItemGridAdapter {
+}
---
+++ Android/app/src/main/java/com/example/bestie/storage/StorageActivity.java
@@ -0,0 +1,15 @@
+package com.example.bestie.storage;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.bestie.R;
+
+public class StorageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_storage);
+    }
+}
