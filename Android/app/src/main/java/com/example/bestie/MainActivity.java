468e8bf11fafa060cc6910f085c4ae5c447a856b
reeccardo <94625621+reeccardo@users.noreply.github.com>
2022-02-24 16:34:52 +0100

    activity collegate nel drawer menù

--- Android/app/src/main/java/com/example/bestie/MainActivity.java
+++ Android/app/src/main/java/com/example/bestie/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Button;
@@ -13,8 +14,12 @@ import android.widget.Button;
 import com.example.bestie.curiosity.CuriosityFragment;
 import com.example.bestie.home.HomeFragment;
 import com.example.bestie.map.MapFragment;
+import com.example.bestie.pet.NewPetActivity;
+import com.example.bestie.settings.SettingsActivity;
+import com.google.android.gms.auth.api.signin.internal.Storage;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
         Button menuButton = findViewById(R.id.menu_button);
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
         Toolbar toolbar = findViewById(R.id.toolbar);
+        NavigationView navigationView=findViewById(R.id.nav_view);
         //DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
 
         setSupportActionBar(toolbar);
@@ -49,6 +55,26 @@ public class MainActivity extends AppCompatActivity {
                         break;
                 }
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selected).commit();
+                return true;
+            }
+        });
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Intent selected = null;
+                switch (item.getItemId()){
+                    case R.id.add_pet:
+                        selected=new Intent(MainActivity.this, NewPetActivity.class);
+                        break;
+                   /* case R.id.archivio:
+                        selected=new Intent(MainActivity.this, StorageActivity.class);
+                        break;*/
+                    case R.id.settings:
+                        selected=new Intent(MainActivity.this, SettingsActivity.class);
+                        break;
+                }
+                startActivity(selected);
                 return true;
             }
         });
