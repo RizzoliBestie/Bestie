6879903a827c366dedc3066fd8ba169f462f9ddd
reeccardo <94625621+reeccardo@users.noreply.github.com>
2022-03-01 11:49:28 +0100

    fix new_pet.xml

--- Android/app/src/main/java/com/example/bestie/pet/NewPetActivity.java
+++ Android/app/src/main/java/com/example/bestie/pet/NewPetActivity.java
@@ -11,7 +11,7 @@ public class NewPetActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_new_pet);
+        setContentView(R.layout.activity_new_pet);
     }
 
     @Override
