c5d3710408335aadfd9f3a273c14e4eb345fd145
reeccardo <94625621+reeccardo@users.noreply.github.com>
2022-03-01 17:24:33 +0100

    aggiunta classe per item default dello spinner

--- Android/app/src/main/java/com/example/bestie/pet/NewPetActivity.java
+++ Android/app/src/main/java/com/example/bestie/pet/NewPetActivity.java
@@ -1,7 +1,5 @@
 package com.example.bestie.pet;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
@@ -11,7 +9,7 @@ import com.example.bestie.R;
 
 public class NewPetActivity extends Activity {
 
-    String peso[] = new String[201];
+    String[] peso = new String[201];
 
     public void caricaPeso(){
         for (int i=0; i<200; i++){
@@ -20,21 +18,33 @@ public class NewPetActivity extends Activity {
         peso[200]= "200+";
     }
 
+    //String[] specie= new String[]{"Seleziona la specie","Cane", "Gatto", "Roditore", "Pesce", "Rettile", "Volatile"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_pet);
-
-        Spinner spinnerPeso = (Spinner) findViewById(R.id.weight);
         caricaPeso();
+
+        Spinner specieACTV = (Spinner) findViewById(R.id.specieET);
+        Spinner spinnerPeso = (Spinner) findViewById(R.id.weight);
+
 
         ArrayAdapter pesoAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, peso);
         pesoAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerPeso.setAdapter(pesoAdapter);
+
+
+        ArrayAdapter<CharSequence> specieAdapter = ArrayAdapter.createFromResource(this, R.array.species, android.R.layout.simple_dropdown_item_1line);
+        specieAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        NothingSelectedSpinnerAdapter adapterOK = new NothingSelectedSpinnerAdapter(specieAdapter, R.layout.species_spinner_nothing_selected, this);
+        specieACTV.setAdapter(adapterOK);
     }
 
     @Override
     public void onBackPressed() {
         super.onBackPressed();
     }
+
+
 }
