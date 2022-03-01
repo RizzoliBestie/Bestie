b0af979dea5cb313b0dbba9fb06af7f57ccdae25
reeccardo <94625621+reeccardo@users.noreply.github.com>
2022-03-01 12:57:24 +0100

    update activity_new_pet.xml
    manca ancora un po' di roba

--- Android/app/src/main/java/com/example/bestie/pet/NewPetActivity.java
+++ Android/app/src/main/java/com/example/bestie/pet/NewPetActivity.java
@@ -2,16 +2,36 @@ package com.example.bestie.pet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.bestie.R;
 
-public class NewPetActivity extends AppCompatActivity {
+public class NewPetActivity extends Activity {
+
+    String peso[] = new String[202];
+
+    public void caricaPeso(){
+        peso[0]="Seleziona peso";
+        for (int i=1; i<=200; i++){
+            peso[i]= Integer.toString(i);
+        }
+        peso[201]= "200+";
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_pet);
+
+        Spinner spinnerPeso = (Spinner) findViewById(R.id.weight);
+        caricaPeso();
+
+        ArrayAdapter pesoAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, peso);
+        pesoAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerPeso.setAdapter(pesoAdapter);
     }
 
     @Override
