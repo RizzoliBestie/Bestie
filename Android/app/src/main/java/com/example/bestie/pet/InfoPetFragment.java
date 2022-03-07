db2bee7c21106ca894f37736682dc410ea19eca7
reeccardo <94625621+reeccardo@users.noreply.github.com>
2022-03-07 17:53:03 +0100

    aggiunto fragment per pet activity
    ancora non funziona ma è un inizio

---
+++ Android/app/src/main/java/com/example/bestie/pet/InfoPetFragment.java
@@ -0,0 +1,67 @@
+package com.example.bestie.pet;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CalendarView;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.bestie.R;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class InfoPetFragment extends Fragment {
+    ArrayList<InfoPetListItem> infoArrayList= new ArrayList<>();
+    Activity act=null;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        act = (Activity) context;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_info_pet, container, false);
+
+        ListView infoPetLV = (ListView) view.findViewById(R.id.infoPetLV);
+        CalendarView calendarView = view.findViewById(R.id.info_pet_date);
+        loadArray(infoArrayList);
+        PetListAdapter adapter = new PetListAdapter(act, R.layout.info_pet_list_item, R.id.info_pet_title, infoArrayList);
+        infoPetLV.setAdapter(adapter);
+
+        calendarView.setDate(new Date().getTime());
+
+        return view;
+    }
+
+
+
+
+
+    //METODO PER CARICARE ARRAY
+    ArrayList<InfoPetListItem> loadArray(ArrayList<InfoPetListItem> arrayList){
+        InfoPetListItem specie = new InfoPetListItem("Specie", null);
+        InfoPetListItem razza = new InfoPetListItem("Razza", null);
+        InfoPetListItem peso = new InfoPetListItem("Peso", null);
+        InfoPetListItem pelo = new InfoPetListItem("Pelo", null);
+        InfoPetListItem sterile = new InfoPetListItem("Sterilizzazione", null);
+
+        arrayList.add(specie);
+        arrayList.add(razza);
+        arrayList.add(peso);
+        arrayList.add(pelo);
+        arrayList.add(sterile);
+
+        return arrayList;
+    }
+}
--- Android/app/src/main/java/com/example/bestie/pet/PetActivity.java
+++ Android/app/src/main/java/com/example/bestie/pet/PetActivity.java
@@ -6,42 +6,28 @@ import androidx.appcompat.widget.Toolbar;
 import android.icu.text.IDNA;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.CalendarView;
 import android.widget.ListView;
 
 import com.example.bestie.R;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class PetActivity extends AppCompatActivity {
 
-    ArrayList<InfoPetListItem> infoArrayList= new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pet);
 
-        ListView infoPetLV = (ListView) findViewById(R.id.infoPetLV);
+        
         Toolbar toolbar = findViewById(R.id.pet_toolbar);
         setSupportActionBar(toolbar);
 
-        loadArray(infoArrayList);
-        PetListAdapter adapter = new PetListAdapter(this, R.layout.info_pet_list_item, R.id.info_pet_title, infoArrayList);
-        infoPetLV.setAdapter(adapter);
+
     }
 
-        ArrayList<InfoPetListItem> loadArray(ArrayList<InfoPetListItem> arrayList){
-        InfoPetListItem specie = new InfoPetListItem("Specie", null);
-        InfoPetListItem razza = new InfoPetListItem("Razza", null);
-        InfoPetListItem peso = new InfoPetListItem("Peso", null);
-        InfoPetListItem pelo = new InfoPetListItem("Pelo", null);
-        InfoPetListItem sterile = new InfoPetListItem("Sterilizzazione", null);
 
-        arrayList.add(specie);
-        arrayList.add(razza);
-        arrayList.add(peso);
-        arrayList.add(pelo);
-        arrayList.add(sterile);
-        
-        return arrayList;
-    }
+
 }
