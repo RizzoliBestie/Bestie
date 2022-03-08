908794be7cc2b17f9514e9b90f552ce8957e41b8
reeccardo <94625621+reeccardo@users.noreply.github.com>
2022-03-08 15:22:21 +0100

    pet activity quasi finita
    mancano controlli e possibilità di modificare i campi.
    ora è possibile visualizzare tutte le informazioni del pet, anche se ancora cablate nel codice

--- Android/app/src/main/java/com/example/bestie/pet/InfoPetFragment.java
+++ Android/app/src/main/java/com/example/bestie/pet/InfoPetFragment.java
@@ -2,11 +2,13 @@ package com.example.bestie.pet;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CalendarView;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import androidx.annotation.NonNull;
@@ -23,6 +25,13 @@ public class InfoPetFragment extends Fragment {
     ArrayList<InfoPetListItem> infoArrayList= new ArrayList<>();
     ArrayList<InfoPetListItem> infoArrayList2= new ArrayList<>();
     Activity act=null;
+    boolean isMale;
+    String maleOnUri = "@drawable/male_on";
+    String maleUri = "@drawable/male";
+    String femaleOnUri = "@drawable/female_on";
+    String femaleUri = "@drawable/female";
+    int imageResourceM;
+    int imageResourceF;
 
     @Override
     public void onAttach(@NonNull Context context) {
@@ -38,6 +47,8 @@ public class InfoPetFragment extends Fragment {
         ListView infoPetLV = (ListView) view.findViewById(R.id.infoPetLV);
         ListView infoPetLV2 = (ListView) view.findViewById(R.id.infoPetLV2);
         CalendarView calendarView = view.findViewById(R.id.info_pet_date);
+        ImageView male = view.findViewById(R.id.info_pet_male);
+        ImageView female = view.findViewById(R.id.info_pet_female);
 
         loadArray(infoArrayList);
         PetListAdapter adapter = new PetListAdapter(act, R.layout.info_pet_list_item, infoArrayList);
@@ -48,6 +59,62 @@ public class InfoPetFragment extends Fragment {
         infoPetLV2.setAdapter(adapter2);
 
         calendarView.setDate(new Date().getTime());
+
+        isMale=false;
+        //SETTO IMMAGINI SE MASCHIO O FEMMINA
+        if(isMale){
+            imageResourceM = getResources().getIdentifier(maleOnUri, null, act.getPackageName());
+            Drawable drawableM = getResources().getDrawable(imageResourceM, null);
+            male.setImageDrawable(drawableM);
+
+            imageResourceF = getResources().getIdentifier(femaleUri, null, act.getPackageName());
+            Drawable drawableF = getResources().getDrawable(imageResourceF, null);
+            female.setImageDrawable(drawableF);
+        }
+        else{
+            imageResourceM = getResources().getIdentifier(maleUri, null, act.getPackageName());
+            Drawable drawableM = getResources().getDrawable(imageResourceM, null);
+            male.setImageDrawable(drawableM);
+
+            imageResourceF = getResources().getIdentifier(femaleOnUri, null, act.getPackageName());
+            Drawable drawableF = getResources().getDrawable(imageResourceF, null);
+            female.setImageDrawable(drawableF);
+        }
+
+        //SETTO METODO ON CLICK PER CAMBIARE SESSO (???)
+        male.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!isMale){
+                    imageResourceM = getResources().getIdentifier(maleOnUri, null, act.getPackageName());
+                    Drawable drawableM = getResources().getDrawable(imageResourceM, null);
+                    male.setImageDrawable(drawableM);
+
+                    imageResourceF = getResources().getIdentifier(femaleUri, null, act.getPackageName());
+                    Drawable drawableF = getResources().getDrawable(imageResourceF, null);
+                    female.setImageDrawable(drawableF);
+
+                    isMale=true;
+                }
+            }
+        });
+
+        female.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(isMale){
+                    imageResourceM = getResources().getIdentifier(maleUri, null, act.getPackageName());
+                    Drawable drawableM = getResources().getDrawable(imageResourceM, null);
+                    male.setImageDrawable(drawableM);
+
+                    imageResourceF = getResources().getIdentifier(femaleOnUri, null, act.getPackageName());
+                    Drawable drawableF = getResources().getDrawable(imageResourceF, null);
+                    female.setImageDrawable(drawableF);
+
+                    isMale=false;
+                }
+            }
+        });
 
         return view;
     }
